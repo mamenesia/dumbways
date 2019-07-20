c29d4160ee566af441659b4d5757c2dfbd284534
mamenesia <dedy.prasetyo.h@gmail.com>
2019-07-20 20:21:06 +0700

    last check

--- Soal 4/soal4.js
+++ Soal 4/soal4.js
@@ -3,9 +3,10 @@ function count_handshake(n) {
     // jika tidak ada orang atau orang sendirian tidak ada jabat tangan, kecuali jabat tangan sendiri.
     return 0
   } else {
-    // total jabat tangan 1 orang di tambah total jabat tangan orang lainnya.
-    return (n-1) + (count_handshake(n-1));
+    // total jabat tangan 1 orang di tambah total jabat tangan orang lainnya. Fun Recurcion.
+    return (n-1) + (count_handshake(n-1)); 
   }
+
 }
 
 count_handshake(3);
